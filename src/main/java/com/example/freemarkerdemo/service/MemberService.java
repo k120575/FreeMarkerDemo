40e1b1c10467f9b382b17039289e3a4c973d8b5f
Kevin <kevin.liao@ambernet.com.tw>
2021-09-23 16:58:24 +0800

    增加member實作

--- src/main/java/com/example/freemarkerdemo/service/MemberService.java
+++ src/main/java/com/example/freemarkerdemo/service/MemberService.java
@@ -1,9 +1,10 @@
 package com.example.freemarkerdemo.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 @Service
 public interface MemberService {
 
-    void addMember(String name, String password, Integer level);
+    ModelAndView addMember(String name, String password, Integer level);
 }
---
+++ src/main/java/com/example/freemarkerdemo/service/impl/MemberServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.freemarkerdemo.service.impl;
+
+import com.example.freemarkerdemo.pojo.Member;
+import com.example.freemarkerdemo.repository.MemberRepository;
+import com.example.freemarkerdemo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Override
+    public ModelAndView addMember(String name, String password, Integer level) {
+        Member member = memberRepository.findByName(name);
+        if (Objects.isNull(member)){
+            Member member1 = new Member(name, password, level);
+            member1.setCreateTime(LocalDateTime.now());
+            memberRepository.saveAndFlush(member1);
+        } else {
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.addObject("errorMsg", "註冊過了");
+            return modelAndView;
+        }
+        return null;
+    }
+}
