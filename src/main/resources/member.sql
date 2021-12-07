820ec37201b8e612c8ca4c8af10c90eac6fbee1a
Kevin <kevin.liao@ambernet.com.tw>
2021-12-07 14:52:22 +0800

    調整member.sql

--- src/main/resources/member.sql
+++ src/main/resources/member.sql
@@ -1,11 +1,11 @@
 Drop TABLE IF EXISTS 'member';
 CREATE TABLE `member` (
-                          `id` int NOT NULL AUTO_INCREMENT,
-                          `name` varchar(45) DEFAULT NULL,
-                          `password` varchar(45) DEFAULT NULL,
-                          `level` int DEFAULT NULL,
-                          `create_time` datetime DEFAULT NULL,
-                          `create_user` varchar(45) DEFAULT NULL,
-                          `score` decimal(10,0) DEFAULT NULL,
-                          PRIMARY KEY (`id`)
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(45) DEFAULT NULL,
+    `password` varchar(45) DEFAULT NULL,
+    `level` int DEFAULT NULL,
+    `score` decimal(10,2) DEFAULT NULL,
+    `create_time` datetime DEFAULT NULL,
+    `create_user` varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
